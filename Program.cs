4a819eece59e6c502c4fbf420ac21af33267a463
Qui <lquiitfuture@gmail.com>
2019-12-24 21:31:45 +0700

    add Project

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExeptionHandling
+{
+    class Program
+    {
+        // The below example shows program execution without exception
+      /*  static void Main(string[] args)
+        {      
+                int a = 20;
+                int b = 10;
+                int c;
+                Console.WriteLine("A VALUE = " + a);
+                Console.WriteLine("B VALUE = " + b);
+                c = a / b;
+                Console.WriteLine("C VALUE = " + c);
+                Console.ReadKey();    
+        }
+        */
+        //The below example shows program execution with the exception
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 0;
+            int c;
+            Console.WriteLine("A VALUE = " + a);
+            Console.WriteLine("B VALUE = " + b);
+            c = a / b;
+            Console.WriteLine("C VALUE = " + c);
+            Console.ReadKey();
+        }
+
+    }
+}
